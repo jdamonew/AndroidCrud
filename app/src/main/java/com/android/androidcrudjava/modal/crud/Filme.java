35ed64f422607b42709a3b7a813eb7f20d153632
jdamonew <damone556@gmail.com>
2019-05-23 23:53:46 -0300

    Gets and Sets - Modal.Filme

--- app/src/main/java/com/android/androidcrudjava/modal/crud/Filme.java
+++ app/src/main/java/com/android/androidcrudjava/modal/crud/Filme.java
@@ -1,9 +1,51 @@
 package com.android.androidcrudjava.modal.crud;
 
 public class Filme {
+
+    Long id;
     String titulo;
     String ano;
     String genero;
     String diretor;
-    //teste
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 }
